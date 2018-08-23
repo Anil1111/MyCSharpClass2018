1ed177c6b94a6b30671074fc68036f9c89f33157
Chris Roberts <clroberts1138@gmail.com>
2018-08-22 21:15:55 -0500

    Initial Project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+        Console.WriteLine("Hello World!");
+        // This is a single line comment
+        /* This is a multiline comment
+            It will treat everything in between
+            the slash asterisk and asterisk slash
+            are comments.
+             */
+        }
+    }
+}
